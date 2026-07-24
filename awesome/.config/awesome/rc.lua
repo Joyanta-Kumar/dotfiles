39c8069788d7fd974ab32b3f825b3a69e902933f
Joyanta Kumar <joyroy122004@gmail.com>
2026-07-24 21:34:48 +0600

    Added picom

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -606,7 +606,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 
 
--- awful.spawn.with_shell("picom")
+awful.spawn.with_shell("picom")
 awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 -- Autostart Redshift
 awful.spawn.with_shell("pgrep -x redshift || redshift &")
