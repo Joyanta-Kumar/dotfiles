39f66f236e013ea9c0f1acc96c3544d0344f1ed0
Joyanta Kumar <joyroy122004@gmail.com>
2026-07-31 12:03:15 +0600

    Making things better

---
+++ hyprland/.config/hypr/modules/monitors.lua
@@ -0,0 +1,11 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+    output   = "HDMI-A-2",
+    mode     = "preferred",
+    position = "auto",
+    scale    = "1",
+})
