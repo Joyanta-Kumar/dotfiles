e5dc75e4789a5052fd845db4f45024be16af2c9d
Joyanta Kumar <joyroy122004@gmail.com>
2026-08-21 13:12:33 +0600

    added snacks for nvim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,20 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  opts = {
+    indent = { enable = true },
+    highlight = { enable = true },
+    folds = { enable = true },
+    ensure_installed = {
+      "c",
+      "html",
+      "lua",
+      "markdown",
+      "python",
+      "toml",
+      "bash",
+    }
+  }
 }
 
