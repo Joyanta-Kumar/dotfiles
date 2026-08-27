73df488211952b3ac111ca431a70eef5255a07ef
Joyanta Kumar <joyroy122004@gmail.com>
2026-08-27 15:30:12 +0600

    Started to understand telescope: it is really good

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,14 @@ return {
   'nvim-telescope/telescope.nvim', version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  }
+    { 'nvim-telescope/telescope-fzf-native.nvim'},
+  },
+
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc="telescope find files"})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc="telescope live grep"})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {desc="telescope buffer"})
+    vim.keymap.set("n", "<leader>fn", builtin.help_tags, {desc="telescope help tags"})
+  end
 }
